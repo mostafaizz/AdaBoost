78ad0aeeae307c6d0a719f77b2bac8a3a5740d58
mostafa.3ez@gmail.com <izz.mostafa@gmail.com>
2016-10-23 09:49:56 -0400

    handling issues in the GUI

--- AdaBoostGUI/MainWindow.xaml.cs
+++ AdaBoostGUI/MainWindow.xaml.cs
@@ -29,6 +29,11 @@ namespace AdaBoostGUI
         public MainWindow()
         {
             InitializeComponent();
+            TextBox[] texts = new TextBox[] { textBoxT, textBoxPatchWidth, textBoxPatchHeight, textBoxNumStages, textBoxNumPos, textBoxNumNeg, textBoxMinRate, textBoxMaxFalse };
+            foreach (TextBox obj in texts)
+            {
+                DataObject.AddPastingHandler(obj, PasteHandler);
+            }
         }
 
         [DllImport("opencv31_2015.dll", CallingConvention = CallingConvention.Cdecl)]
@@ -43,6 +48,63 @@ namespace AdaBoostGUI
 
         [DllImport("opencv31_2015.dll")]
         public static extern IntPtr faceDetect(string fileName, string modelName, out int imgSize);
+
+        // Handling integer inputs
+        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            object[] intTexts = new object[] {textBoxT, textBoxPatchWidth, textBoxPatchHeight, textBoxNumStages, textBoxNumPos, textBoxNumNeg };
+            object[] doubleTexts = new object[] { textBoxMinRate, textBoxMaxFalse };
+            if (Array.Exists<object>(intTexts, element => element == sender))
+            {
+                // Prohibit non-int
+                int i;
+                if (!int.TryParse(e.Text, out i))
+                    e.Handled = true;
+            }
+            else
+            {
+                // Prohibit non-double
+                double i;
+                if (!double.TryParse(e.Text, out i))
+                    e.Handled = true;
+            }
+        }
+
+        private void TextBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Prohibit space
+            if (e.Key == Key.Space)
+                e.Handled = true;
+        }
+
+        private void PasteHandler(object sender, DataObjectPastingEventArgs e)
+        {
+            TextBox tb = sender as TextBox;
+            bool textOK = false;
+
+            if (e.DataObject.GetDataPresent(typeof(string)))
+            {
+                // Allow pasting only alphabetic
+                string pasteText = e.DataObject.GetData(typeof(string)) as string;
+                object[] intTexts = new object[] { textBoxT, textBoxPatchWidth, textBoxPatchHeight, textBoxNumStages, textBoxNumPos, textBoxNumNeg };
+                object[] doubleTexts = new object[] { textBoxMinRate, textBoxMaxFalse };
+                if (Array.Exists<object>(intTexts, element => element == sender))
+                {
+                    int i;
+                    if (int.TryParse(pasteText, out i))
+                        textOK = true;
+                }
+                else
+                {
+                    double i;
+                    if (double.TryParse(pasteText, out i))
+                        textOK = true;
+                }
+            }
+
+            if (!textOK)
+                e.CancelCommand();
+        }
 
         private BitmapImage getImageFromBytes(byte[] buf)
         {
@@ -85,46 +147,13 @@ namespace AdaBoostGUI
             int h = ((bool)checkBoxHorizontal.IsChecked) ? 1 : 0;
             int v = ((bool)checkBoxVertical.IsChecked) ? 1 : 0;
             int T = int.Parse(textBoxT.Text);
-
-            /*int r = 0;
-            IntPtr ptr = test("orig.png", out r);
-            byte []buf = new byte[r];
-            Marshal.Copy(ptr, buf,0 , r);
-            try
-            {
-                //byte [] buf1 = File.ReadAllBytes("orig.png");
-                MemoryStream memoryStream = new MemoryStream(buf);
-
-                BitmapImage bitmap = new BitmapImage();
-                bitmap.BeginInit();
-                bitmap.DecodePixelWidth = 400;
-                bitmap.DecodePixelHeight = 400;
-                bitmap.StreamSource = memoryStream;
-                bitmap.EndInit();
-                bitmap.Freeze();
-
-                imageTrain.Source = bitmap;
-            }
-            catch (Exception ex)
-            {
-                string message = ex.Message;
-            }
-            */
-            //var img = Image.(stream);
-            //BitmapImage img = new BitmapImage(new Uri("test.png"));
-            //image.Source = img;
             IntPtr trainImg, testImg;
             int trainImgSize, testImgSize;
             testAdaBoostEdgeDetection(h, v, T, textBoxIn.Text, textBoxOut.Text, out trainImg, out trainImgSize, out testImg, out testImgSize);
             imageTrain.Source = getImageFromIntPtr(trainImg, trainImgSize);
             imageTest.Source = getImageFromIntPtr(testImg, testImgSize);
         }
-
-        private void checkBox_Checked(object sender, RoutedEventArgs e)
-        {
-
-        }
-
+        
         private string openPngFile()
         {
             return openFile("PNG Files (*.png)|*.png");
@@ -205,18 +234,45 @@ namespace AdaBoostGUI
         }
 
         int stage = 0;
+        BackgroundWorker bkgWorker = null;
         private void buttonTrain_Click(object sender, RoutedEventArgs e)
         {
-            textBlock.Text = "";
-            BackgroundWorker bkgWorker = new BackgroundWorker();
+            if (buttonTrain.Content.ToString() == "Start Training")
+            {
+                bkgWorker = new BackgroundWorker();
+                buttonTrain.Content = "Cancel Training";
+                textBlock.Text = "";
+                pBar.Value = 0;
+                stage = 0;
+            }
+            else
+            {
+                MessageBoxResult result = MessageBox.Show("Do you want to cancel this training?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (result == MessageBoxResult.Yes)
+                {
+                    //bkgWorker.CancelAsync();
+                    exeProcess.CloseMainWindow();
+                    exeProcess.Kill();
+                    exeProcess.WaitForExit();
+                    bkgWorker.CancelAsync();
+                    //
+                    buttonTrain.Content = "Start Training";
+                    textBlock.Text = "";
+                    pBar.Value = 0;
+                }
+                return;
+            }
+
             
+            bkgWorker.WorkerSupportsCancellation = true;
             bkgWorker.DoWork += Worker_DoWork;
             bkgWorker.ProgressChanged += Worker_ProgressChanged;
+            
             bkgWorker.WorkerReportsProgress = true;
             string args = "";
             args += string.Format("-data {0} -vec {1} -bg {2} ", labelOutput.Text, labelPos.Text, labelNeg.Text);
             args += string.Format("-precalcValBufSize {0} -precalcIdxBufSize {1}  ", 2048, 2048);
-            args += string.Format("-numPos {0} -numNeg {1} -nstages {2} ", textBoxNumPos.Text, textBoxNumNeg.Text, textBoxNumStages.Text);
+            args += string.Format("-numPos {0} -numNeg {1} -numStages {2} ", textBoxNumPos.Text, textBoxNumNeg.Text, textBoxNumStages.Text);
             args += string.Format("-minhitrate {0} -maxfalsealarm {1} ", textBoxMinRate.Text, textBoxMaxFalse.Text);
             args += string.Format("-w {0} -h {1} -nonsym -baseFormatSave ", textBoxPatchWidth.Text, textBoxPatchHeight.Text);
 
@@ -235,6 +291,8 @@ namespace AdaBoostGUI
                 if(txt == "MY_END")
                 {
                     pBar.Value = pBar.Maximum;
+                    buttonTrain.Content = "Start Training";
+                    textBlock.ScrollToEnd();
                 }
                 else
                 {
@@ -242,17 +300,21 @@ namespace AdaBoostGUI
                     {
                         int total = int.Parse(textBoxNumStages.Text);
 
-                        pBar.Value = ((++stage) * 1.0 / total) * pBar.Maximum;
+                        pBar.Value = ((stage++) * 1.0 / total) * pBar.Maximum;
 
                         //textBlock.Focus();
                         //textBlock.CaretIndex = textBlock.Text.Length;
-                        textBlock.ScrollToEnd();
+                        
                     }
                     textBlock.Text += txt + "\r\n";
+                    if (e.ProgressPercentage % 20 == 0)
+                    {
+                        textBlock.ScrollToEnd();
+                    }
                 }
             }
         }
-
+        Process exeProcess = null;
         private void Worker_DoWork(object sender, DoWorkEventArgs e)
         {
 
@@ -274,14 +336,15 @@ namespace AdaBoostGUI
                 int i = 0;
                 // Start the process with the info we specified.
                 // Call WaitForExit and then the using statement will close.
-                using (Process exeProcess = Process.Start(startInfo))
+                using (exeProcess = Process.Start(startInfo))
                 {
                     exeProcess.OutputDataReceived += ExeProcess_OutputDataReceived;
                     while (!exeProcess.StandardOutput.EndOfStream)
                     {
+
                         string line = exeProcess.StandardOutput.ReadLine();
                         // do something with line
-                        (sender as BackgroundWorker).ReportProgress(i++,line);
+                        (sender as BackgroundWorker).ReportProgress(i++, line);
                     }
                     exeProcess.WaitForExit();
                     (sender as BackgroundWorker).ReportProgress(0, "MY_END");
